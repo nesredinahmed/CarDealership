142585640444c0cdc312823a03a934b4b6c16e16
Kevin Ernest Long <kevinelong@gmail.com>
2025-05-09 11:48:53 -0700

    wip

--- src/Dealership.java
+++ src/Dealership.java
@@ -1,55 +1,67 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Dealership{
+class Dealership {
     private String name;
     private String address;
     private String phone;
     private ArrayList<Vehicle> inventory;
 
     //CONSTRUCTOR
-    Dealership(String name, String address, String phone){
+    Dealership(String name, String address, String phone) {
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.inventory = new ArrayList<Vehicle>();
     }
-    public String toString(){
+
+    public String toString() {
         return String.format(
-                "DEALERSHIP NAME: '%s' #CARS: %d\n", this.name, this.inventory.size()
+                "DEALERSHIP NAME: '%s' #CARS: %d\n",
+                this.name, this.inventory.size()
         );
     }
+
     //METHODS
-    public List<Vehicle> getVehiclesByPrice(double min, double max){
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
         return inventory;
     }
-    public List<Vehicle> getVehiclesByMakeModel(String make, String Model){
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String Model) {
         return inventory;
     }
-    public List<Vehicle> getVehiclesByYear(int min, int max){
+
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
         return inventory;
     }
-    public List<Vehicle> getVehiclesByColor(String color){
+
+    public List<Vehicle> getVehiclesByColor(String color) {
         return inventory;
     }
-    public List<Vehicle> getVehiclesByMileage(int min, int max){
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
         return inventory;
     }
-    public List<Vehicle> getVehiclesByType(String type){
+
+    public List<Vehicle> getVehiclesByType(String type) {
         return inventory;
     }
-    public List<Vehicle> getAllVehicles(){
+
+    public List<Vehicle> getAllVehicles() {
         return inventory;
     }
-    Vehicle addVehicle(Vehicle v){
+
+    Vehicle addVehicle(Vehicle v) {
         inventory.add(v);
         return v;
     }
-    void removeVehicle(Vehicle v){
+
+    void removeVehicle(Vehicle v) {
         inventory.remove(v);
     }
+
     public static void main(String[] args) {
-        var d = new Dealership("XYZ MOTORS", "","");
+        var d = new Dealership("XYZ MOTORS", "", "");
 
         //ADD TO INVENTORY
         d.addVehicle(new Vehicle(123, 1999, "MAZDA", "MX-5", "ROADSTER", "WHITE", 128_000, 5_000));
@@ -57,7 +69,7 @@ class Dealership{
 
         System.out.println(d); //show dealership
 
-        for(Vehicle v : d.getAllVehicles()){
+        for (Vehicle v : d.getAllVehicles()) {
             System.out.println(v);
         }
     }
